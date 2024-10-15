d80ee5713f32171b8256699fb7681734dbdbe363
cucumeris <germanb0unz3r@gmail.com>
2024-10-15 11:33:21 +0200

    removed unused imports

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,8 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import styles from './ProductList.module.css'
 import ProductCard from './ProductCard/ProductCard';
 import ProductData from 'utils/scripts/ProductData';
-import { AppContext } from 'context/AppContext';
 
 interface ProductListProps {
 	products: ProductData[];
